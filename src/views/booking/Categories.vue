e7c7ce6ca2c2939be96683dfd38406c0c2e5e86a
1051328571@qq.com <897665728Asd>
2020-01-26 10:01:17 +0800

    记账页面

---
+++ src/views/booking/Categories.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="category">
+        <ul>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+            <li>呵呵哒</li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: ""
+    }
+</script>
+
+<style scoped>
+    .category{
+        height: 100px;
+        width: 100%;
+        background-color: aquamarine;
+        overflow-y: scroll;
+
+    }
+
+</style>
